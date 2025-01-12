cae92dad0a11d81bd33eb00ca9019886ed9a80c4
JungYi Bae <jungyi.bae@gmail.com>
2025-01-12 22:16:12 +0100

    first commit

---
+++ algorithmProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithmProject
+//
+//  Created by 배정이 on 1/12/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
