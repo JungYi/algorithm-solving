4c7269b5220b6ffa5b41a72e1e079e7044a14771
JungYi Bae <jungyi.bae@gmail.com>
2026-01-30 23:30:24 +0100

    30. 01. 2026 LeetCode_392_IsSubsequence

---
+++ javascript/LeetCode/LeetCode_392_IsSubsequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    if (s.length === 0) return true;
+
+    let sIdx = 0;
+    for (let i = 0; i < t.length; i++) {
+        if (s[sIdx] === t[i]) sIdx++;
+
+        if (sIdx === s.length) return true;
+    }
+    return false;
+};
