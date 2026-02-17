30b26812d611dfe48365ac31be7cec0d317649d0
JungYi Bae <jungyi.bae@gmail.com>
2026-02-17 18:02:38 +0100

    17. 02. 2026 LeetCode_035_SearchInsertPosition.java

---
+++ Java/LeetCode/LeetCode_035_SearchInsertPosition.java
@@ -0,0 +1,29 @@
+public class LeetCode_035_SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left <= right) {
+            int mid = (left+right) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if (nums[mid] < target) left = mid+1;
+            else right = mid-1;
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        LeetCode_035_SearchInsertPosition s = new LeetCode_035_SearchInsertPosition();
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        System.out.println(s.searchInsert(nums, target));
+    }
+    
+    /**
+     * cd Java/LeetCode
+     * javac LeetCode_416_PartitionEqualSubsetSum.java
+     * java LeetCode_416_PartitionEqualSubsetSum
+     */
+}
